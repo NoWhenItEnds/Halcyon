57c0498779a16f5322cb2e8c112900b755d94a03
James Penn <JamesLockyerPenn@gmail.com>
2026-03-07 08:21:03 +0800

    feat: Added dice random.

---
+++ Systems/Utilities/DiceRandom.cs
@@ -0,0 +1,81 @@
+using System;
+
+namespace Warlord.Utilities
+{
+    /// <summary> An implementation of random simulating a dice pool. </summary>
+    public class DiceRandom
+    {
+        /// <summary> The underlying random class to use for generating random numbers. </summary>
+        private readonly Random RANDOM;
+
+
+        /// <summary> An implementation of random simulating a dice pool. </summary>
+        public DiceRandom()
+        {
+            RANDOM = new Random();
+        }
+
+
+        /// <summary> An implementation of random simulating a dice pool. </summary>
+        /// <param name="random"> The underlying random class to use for generating random numbers. </param>
+        public DiceRandom(Random random)
+        {
+            RANDOM = random;
+        }
+
+
+        /// <summary> An implementation of random simulating a dice pool. </summary>
+        /// <param name="seed"> The seed to use for the random generator. </param>
+        public DiceRandom(Int32 seed)
+        {
+            RANDOM = new Random(seed);
+        }
+
+
+        /// <summary> Perform a standard test where meeting for exceeding the target number will result in a success. </summary>
+        /// <param name="poolSize"> The number of dice in the pool. </param>
+        /// <param name="targetNumber"> The minimum number on a ten-sided dice the roll needs to equal or succeed to be considered a success. </param>
+        /// <param name="successThreshold"> How many successes the test requires to be met to considered successful. </param>
+        /// <param name="doesExplode"> Whether rerolls are allowed on a ten. </param>
+        /// <returns> How many successes above, or below the target number. A zero indicates a success, with no extra successes. </returns>
+        /// <exception cref="ArgumentOutOfRangeException"/>
+        public Int32 StandardTest(Int32 poolSize, Int32 targetNumber = 8, Int32 successThreshold = 1, Boolean doesExplode = true)
+        {
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(poolSize);
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(successThreshold);
+            ArgumentOutOfRangeException.ThrowIfGreaterThan(successThreshold, 10);
+
+            Int32 successes = 0;
+            for (Int32 i = 0; i < poolSize; i++)
+            {
+                Int32 result = RANDOM.Next(1, 11);
+                if (result >= targetNumber)
+                {
+                    successes++;
+                }
+
+                if (doesExplode && result == 10)
+                {
+                    poolSize++;
+                }
+            }
+
+            return successes - successThreshold;
+        }
+
+
+        /// <summary> Perform a contested test against another. The target number is the result of the contesting result. </summary>
+        /// <param name="poolSize"> The number of dice in the initiator's pool. </param>
+        /// <param name="otherPoolSize"> The number of dice in the defender's pool. </param>
+        /// <param name="successThreshold"> The minimum number on a ten-sided dice the initiator's roll needs to equal or succeed to be considered a success. </param>
+        /// <param name="otherSuccessThreshold"> The minimum number on a ten-sided dice the defender's roll needs to equal or succeed to be considered a success. </param>
+        /// <param name="doesExplode"> Whether the initiator rerolls on a ten. </param>
+        /// <returns> How many successes above, or below the target number. A zero indicates a success, with no extra successes. </returns>
+        /// <exception cref="ArgumentOutOfRangeException"/>
+        public Int32 ContestedTest(Int32 poolSize, Int32 otherPoolSize, Int32 targetNumber = 8, Int32 otherTargetNumber = 8, Boolean doesExplode = true)
+        {
+            Int32 otherResult = StandardTest(otherPoolSize, otherTargetNumber, doesExplode: false);
+            return StandardTest(poolSize, targetNumber, otherResult, doesExplode);
+        }
+    }
+}
